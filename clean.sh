5d7c5a3aa3aeda1e8f6f6a2a859407acfc5b3f0b
Andrew Scholer <ascholer@chemeketa.edu>
2021-12-16 13:31:19 -0800

    Inprove clean script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
-rm */*/*.user
-rm */*/*.exe
-rm -rf */*/Debug
+find -type d -name Debug -exec rm -rv {} +
+find -type d -name Release -exec rm -rv {} +
+find -type d -name .vscode -exec rm -rv {} +
+find -type f -name *.pro.user -delete
+find -type f -name *.exe -delete
+find -type f -name *.o -delete
