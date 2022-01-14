3a7ac699cd4446e2d5ed1b515b88134c881f148b
Andrew Scholer <ascholer@chemeketa.edu>
2022-01-14 13:51:23 -0800

    Remove cruft, add .vscode to gitignore

--- Week02/Conditionals_3/main.cpp
+++ Week02/Conditionals_3/main.cpp
@@ -4,14 +4,5 @@ using namespace std;
 
 int main()
 {
-    int x = 5;
-
-    //This is going to end badly...
-    if(0 <= x <= 10)
-        cout << "x is between 0 and 10" << endl;
-
-    //This is correct
-    if(0 <= x && x <= 10)
-        cout << "x is between 0 and 10" << endl;
-
+    
 }
