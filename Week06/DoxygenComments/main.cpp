b823d34c4720677b7823295a954bb0257f320ce8
Andrew Scholer <ascholer@chemeketa.edu>
2023-10-27 16:15:53 -0700

    Spelling fixes in coments

--- Week06/DoxygenComments/main.cpp
+++ Week06/DoxygenComments/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 /**
- * @brief Caculates the sum of all the numbers from start to end
+ * @brief Calculates the sum of all the numbers from start to end
  * @param start Value to start from - must be lower than end
  * @param end Last value to count (inclusive)
  * @return Sum of all values
@@ -20,6 +20,7 @@ int sum(int start, int end) {
  *              Examples: Jan 1st = 0, Feb 1st = 31
  * @param range How many days in addition to start day to search
  * @return Max temp in degrees C
+ * 
  */
 double getMaxTemperature(int start, int range);
 
@@ -33,7 +34,7 @@ void printAsMoney(double amount) {
 }
 
 /**
- * @brief Get the high temperature (in Fahrenheight) for
+ * @brief Get the high temperature (in Fahrenheit) for
  * the specified day of the year
  * @param day Day of the year. Jan 1st = 1, Feb 1st = 32...
  * @return degrees F
